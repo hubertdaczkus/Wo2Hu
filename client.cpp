0624298a1e06b3149ec2261b9b107771eec22a0d
hubertdaczkus <hubert.daczkus@gmail.com>
2017-01-07 20:13:04 +0100

    Starting point

--- client.cpp
+++ client.cpp
@@ -1,62 +1,34 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
-#define BUF_SIZE 1024
+#define BUFROZ 256
 
-int main(int argc, char* argv[])
-{
-   int nSocket;
-   int nConnect;
-   int nBytes;
-   struct sockaddr_in stServerAddr;
-   struct hostent* lpstServerEnt;
-   char cbBuf[BUF_SIZE];
-
-   if (argc != 3)
-   {
-     fprintf(stderr, "Usage: %s server_name port_number\n", argv[0]);
-     exit(1);
-   }
-
-   /* look up server's IP address */
-   lpstServerEnt = gethostbyname(argv[1]);
-   if (! lpstServerEnt)
-   {
-      fprintf(stderr, "%s: Can't get the server's IP address.\n", argv[0]);
-      exit(1);
-   }
-
-   /* create a socket */
-   nSocket = socket(PF_INET, SOCK_STREAM, 0);
-   if (nSocket < 0)
-   {
-      fprintf(stderr, "%s: Can't create a socket.\n", argv[0]);
-      exit(1);
-   }
-
-   /* server info */
-   memset(&stServerAddr, 0, sizeof(struct sockaddr));
-   stServerAddr.sin_family = AF_INET;
-   memcpy(&stServerAddr.sin_addr.s_addr, lpstServerEnt->h_addr, lpstServerEnt->h_length);
-   stServerAddr.sin_port = htons(atoi(argv[2]));
-
-   /* connect to the server */
-   nConnect = connect(nSocket, (struct sockaddr*)&stServerAddr, sizeof(struct sockaddr));
-   if (nConnect < 0)
-   {
-      fprintf(stderr, "%s: Can't connect to the server (%s:%i).\n", argv[0], argv[1], atoi(argv[2]));
-      exit(1);
-   }
-
-   /* connection is now established; read from socket */
-   nBytes = read(nSocket, cbBuf, BUF_SIZE);
-   write(1, cbBuf, nBytes);
-
-   close(nSocket);
-   return 0;
+int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		printf("Podaj parametry [hostname] [port]\n");
+		return -1;
+	}	
+	int fd = socket(PF_INET, SOCK_STREAM, 0);
+	//int fd = socket(PF_INET, SOCK_DGRAM, 0);
+	struct sockaddr_in addr;
+	struct hostent* ent = gethostbyname(argv[1]);
+	memcpy(&addr.sin_addr.s_addr, ent -> h_addr, ent -> h_length);
+	addr.sin_family = PF_INET;
+	addr.sin_port = htons(atoi(argv[2]));
+	connect(fd, (struct sockaddr*) &addr, sizeof(addr));
+	char buf[BUFROZ];
+    char* message = "71764";
+    write(fd, message, BUFROZ);
+    int i = read(fd, buf, BUFROZ);
+	buf[i] = '\0';
+	printf("%s\n", buf);
+ 	close(fd);
+	return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,83 +1,104 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <signal.h>
+//#include <sys/types.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+#include <arpa/inet.h>
+//#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
 #include <string.h>
+//#include <signal.h>
+//#include <sys/wait.h>
+//#include <pthread.h>
+//#include <sys/select.h>
 
+#define BUFROZ 256
+#define ERROR(e) { perror(e); exit(EXIT_FAILURE); }
 #define SERVER_PORT 1234
 #define QUEUE_SIZE 5
 
-void childend(int signo)
-{
-   pid_t pid;
-   pid = wait(NULL);
-   printf("\t[end of child process number %d]\n", pid);
-}
+using namespace std;
 
-int main(int argc, char* argv[])
-{
-   int nSocket, nClientSocket;
-   int nBind, nListen;
-   int nFoo = 1, nTmp;
-   struct sockaddr_in stAddr, stClientAddr;
+int main(int argc, char** argv) {
+    char buf[BUFROZ];
+	//string lista[6];
+    char *lista[6];
+	lista[0] = "71764";
+	lista[1] = "Hubert Daczkus";
+	lista[2] = "88686";
+    lista[3] = "Lukasz Dur";
+	lista[4] = "70178";
+	lista[5] = "Michal Woch"; 
+    
+    socklen_t slt;
+    int x, sfd, cfd, fdmax, fda, rc, i, j, on = 1;
+    struct sockaddr_in saddr, caddr;
+    static struct timeval timeout;
+    fd_set mask, rmask, wmask;
 
-   signal(SIGCHLD, childend);
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = INADDR_ANY;
+    saddr.sin_port = htons(SERVER_PORT);
 
-   /* address structure */
-   memset(&stAddr, 0, sizeof(struct sockaddr));
-   stAddr.sin_family = AF_INET;
-   stAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-   stAddr.sin_port = htons(SERVER_PORT);
+    if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) ERROR("socket()");
+    if (setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on)) < 0) ERROR("setsockopt()");
+    if (bind(sfd, (struct sockaddr*)&saddr, sizeof(saddr)) < 0) ERROR("bind()");
+    if (listen(sfd, QUEUE_SIZE) < 0) ERROR("listen()");
 
-   /* create a socket */
-   nSocket = socket(AF_INET, SOCK_STREAM, 0);
-   if (nSocket < 0)
-   {
-       fprintf(stderr, "%s: Can't create a socket.\n", argv[0]);
-       exit(1);
-   }
-   setsockopt(nSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&nFoo, sizeof(nFoo));
+    FD_ZERO(&mask);
+    FD_ZERO(&rmask);
+    FD_ZERO(&wmask);
+    fdmax = sfd;
 
-   /* bind a name to a socket */
-   nBind = bind(nSocket, (struct sockaddr*)&stAddr, sizeof(struct sockaddr));
-   if (nBind < 0)
-   {
-       fprintf(stderr, "%s: Can't bind a name to a socket.\n", argv[0]);
-       exit(1);
-   }
-
-   /* specify queue size */
-   nListen = listen(nSocket, QUEUE_SIZE);
-   if (nListen < 0)
-   {
-       fprintf(stderr, "%s: Can't set queue size.\n", argv[0]);
-   }
-
-   while(1)
-   {
-       /* block for connection request */
-       nTmp = sizeof(struct sockaddr);
-       nClientSocket = accept(nSocket, (struct sockaddr*)&stClientAddr, &nTmp);
-       if (nClientSocket < 0)
-       {
-           fprintf(stderr, "%s: Can't create a connection's socket.\n", argv[0]);
-           exit(1);
-       }
-
-       /* connection */
-       if (! fork())
-       {
-           printf("%s: [connection from %s]\n",
-                  argv[0], inet_ntoa((struct in_addr)stClientAddr.sin_addr));
-           write(nClientSocket, "Hello World!\n", 13);
-           close(nClientSocket);
-           exit(0);
-       }
-   }
-
-   close(nSocket);
-   return(0);
+    while(1) {
+        FD_SET(sfd, &rmask);
+        wmask = mask;
+        timeout.tv_sec = 5 * 60;
+        timeout.tv_usec = 0;
+        if ((rc = select(fdmax+1, &rmask, &wmask, (fd_set*)0, &timeout)) < 0) {
+            ERROR("select()");
+        }
+        if (rc == 0) {
+            cout << "timed out" << endl;
+            continue;
+        }
+        fda = rc;
+        if (FD_ISSET(sfd, &rmask)) {
+            fda -= 1;
+            slt = sizeof(caddr);
+            if ((cfd = accept(sfd, (struct sockaddr*)&caddr, &slt)) < 0) {
+                ERROR("accept()");
+            }
+            printf("new connection: %s\n", inet_ntoa((struct in_addr)caddr.sin_addr));
+            FD_SET(cfd, &mask);
+            if (cfd > fdmax) fdmax = cfd;
+        }
+        for (i = sfd+1; i <= fdmax && fda > 0; i++) {
+            if (FD_ISSET(i, &wmask)) {
+                fda -= 1;
+                j = read(cfd, buf, BUFROZ);
+                x = 0;
+                for (j = 0; j < 6; j++) {
+                    if (strcmp(buf, lista[j]) == 0) {
+                        write(cfd, lista[j+1], 20);
+                        x = 1;			
+                    }
+                }	
+                if (x == 0) {	
+                    write(cfd, "Brak indexu w bazie", 20);
+                }
+                close(i);
+                FD_CLR(i, &mask);
+                if (i == fdmax) {
+                    while(fdmax > sfd && !FD_ISSET(fdmax, &mask)) {
+                        fdmax -= 1;
+                    }
+                }
+            }
+        }
+    }
+    return EXIT_SUCCESS;
 }
